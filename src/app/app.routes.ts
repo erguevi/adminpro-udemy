7ec14abeccd9ac4be9849d6be8e7015c2b89a29b
Ernesto Guerra Villarreal <e.guerra.villarreal@accenture.com>
2019-03-17 18:39:45 -0600

    modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,37 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 
 
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const appRoutes: Routes = [
     
-    { 
-        path: '', 
-        component: PagesComponent,
-        children:[
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'grafica1', component: Graficas1Component },
-            { path: '', redirectTo:'/dashboard', pathMatch:'full' },
-
-        ] 
-    },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', component: NopagefoundComponent },
-
-    //{ path: 'path/:routeParam', component: MyComponent },
-    //{ path: 'staticPath', component: ... },
-    //{ path: '**', component: ... },
-    //{ path: 'oldPath', redirectTo: '/staticPath' },
-    //{ path: ..., component: ..., data: { message: 'Custom' }
 ];
 
 
